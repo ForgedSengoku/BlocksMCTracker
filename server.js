f7ec0dc57ce51645a0df13973f86f3ba7b32ca55
ForgedSengoku <dafebyeye@gmail.com>
2025-02-24 18:09:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // main.js - Combined Express server and Electron main process
-const { app: electronApp, BrowserWindow } = require('electron');
+const { app: electronApp, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const express = require('express');
 const fs = require('fs');
@@ -39,13 +39,11 @@ io.on('connection', (socket) => {
 
   // OG Name Claimer events (for indexog.html)
   socket.on('startNamesniper', (data) => {
-    // Now we only expect targetUuid from the client.
     const { targetUuid } = data;
     const { startSniper, readUserOGStore } = require('./namesniper');
     startSniper(targetUuid, (response) => {
       if (response.type === 'claimed') {
         socket.emit('namesniperClaimed', response.message);
-        // Immediately send the updated OG list.
         socket.emit('ogUsernames', readUserOGStore());
       } else if (response.type === 'alert') {
         socket.emit('namesniperAlert', response.message);
@@ -61,13 +59,11 @@ io.on('connection', (socket) => {
     const { stopSniper } = require('./namesniper');
     stopSniper((response) => {
       socket.emit('namesniperStopped', response.message);
-      // Also update the OG list after stopping.
       const { readUserOGStore } = require('./namesniper');
       socket.emit('ogUsernames', readUserOGStore());
     });
   });
 
-  // Event to get stored OG usernames (for indexog.html)
   socket.on('getOGUsernames', () => {
     const { readUserOGStore } = require('./namesniper');
     const ogStore = readUserOGStore();
@@ -84,6 +80,25 @@ server.listen(3052, () => {
   console.log('Server running on http://localhost:3052');
 });
 
+// Set up IPC listeners for custom alert and confirm dialogs
+ipcMain.on('show-alert', (event, message) => {
+  dialog.showMessageBoxSync({
+    message: message,
+    buttons: ['OK']
+  });
+  event.returnValue = true;
+});
+
+ipcMain.on('show-confirm', (event, message) => {
+  const result = dialog.showMessageBoxSync({
+    message: message,
+    buttons: ['Cancel', 'OK'],
+    cancelId: 0,
+    defaultId: 1
+  });
+  event.returnValue = result === 1;
+});
+
 // Electron: Create the browser window and load the Express server
 let mainWindow;
 function createWindow() {
@@ -92,7 +107,8 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: false,
-      contextIsolation: true
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   });
   mainWindow.loadURL('http://localhost:3052');

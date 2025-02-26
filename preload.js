be9942e325669ebddd0841f1d8a22f27102d3a04
ForgedSengoku <dafebyeye@gmail.com>
2025-02-26 15:57:47 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  alert: (message) => ipcRenderer.sendSync('show-alert', message),
-  confirm: (message) => ipcRenderer.sendSync('show-confirm', message)
-});
